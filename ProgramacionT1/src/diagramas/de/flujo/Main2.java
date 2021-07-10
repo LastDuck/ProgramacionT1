8569f4749796c85decaf70ef1eb13d1776951f75
LastDuck <LastDuck@LAPTOP-4R1IOI4B>
2021-07-09 19:54:59 -0500

    tabla del 7

---
+++ ProgramacionT1/src/diagramas/de/flujo/Main2.java
@@ -0,0 +1,16 @@
+package diagramas.de.flujo;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("La tabla del 7:");
+		for (int i = 1; i <= 12; i ++ ) {
+			int M=7*i;
+			
+			System.out.printf("\n 7 * %d = %d",i,M );
+		}
+	}
+
+}
