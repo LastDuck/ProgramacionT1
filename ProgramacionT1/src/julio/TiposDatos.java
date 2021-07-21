c082da0bff59ab890b148fc6654757293ec62594
LastDuck <LastDuck@LAPTOP-4R1IOI4B>
2021-07-21 18:50:10 -0500

    21/07/2021

--- ProgramacionT1/src/julio/TiposDatos.java
+++ ProgramacionT1/src/julio/TiposDatos.java
@@ -5,6 +5,19 @@ public class TiposDatos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//numerico
+		short cantidadEstudiante=1000;
+		byte entradaCircuito=0;
+		int cantCiudadanos=10000;
+		long cantMundo=1000000000;
+		float sueldo= 156.58f;
+		double saldoBanco=547815421.256;
+		
+		//logico
+		boolean casado=true;
+		
+		//caracter
+		char opcionMenu='a';
 	}
 
 }
