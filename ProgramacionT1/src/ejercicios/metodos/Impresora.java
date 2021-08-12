3dda19802cde90146f67bc92543c3e863deb53c1
LastDuck <LastDuck@LAPTOP-4R1IOI4B>
2021-08-11 20:25:48 -0500

    2021/08/11

---
+++ ProgramacionT1/src/ejercicios/metodos/Impresora.java
@@ -0,0 +1,15 @@
+package ejercicios.metodos;
+
+public class Impresora {
+
+	public String color;
+	
+	public void imprimir(String datosAImprimir ,int letra ) {
+		System.out.println("Logo UCE");
+		System.out.println("**************");
+		System.out.println(datosAImprimir);
+		System.out.println("**************");
+		System.out.println("pie de firma");
+		
+	}
+}
---
+++ ProgramacionT1/src/ejercicios/metodos/Matriculacion.java
@@ -0,0 +1,13 @@
+package ejercicios.metodos;
+
+public class Matriculacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		Impresora impresio1=new Impresora();
+		impresio1.imprimir("\"Programacion1\"18", 10);
+	}
+
+}
