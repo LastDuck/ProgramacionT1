21166c0a13183050746f3cfdedcbd015fc59f6a3
LastDuck <LastDuck@LAPTOP-4R1IOI4B>
2021-08-11 20:29:16 -0500

    1

--- ProgramacionT1/src/ejercicios/metodos/Notas.java
+++ ProgramacionT1/src/ejercicios/metodos/Notas.java
@@ -5,6 +5,8 @@ public class Notas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Impresora impresio1=new Impresora();  //instanciar la clase Impresora
+		impresio1.imprimir("NOtas en proceso", 8);
 	}
 
 }
