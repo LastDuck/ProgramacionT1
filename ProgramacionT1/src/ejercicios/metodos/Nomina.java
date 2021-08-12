4bd9ee4612d73b96b074c15fbc4c1978076cecf5
LastDuck <LastDuck@LAPTOP-4R1IOI4B>
2021-08-11 20:28:16 -0500

    111

--- ProgramacionT1/src/ejercicios/metodos/Nomina.java
+++ ProgramacionT1/src/ejercicios/metodos/Nomina.java
@@ -5,6 +5,9 @@ public class Nomina {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Impresora impresio1=new Impresora();  //instanciar la clase Impresora
+		impresio1.imprimir("juan perez  :1200$", 9);
+		
 	}
 
 }
